15786f9ebfb4830c711a39fea99389fcaba29321
Carlos Eduardo Coy Calero <ccoy@bancolombia.com.co>
2023-07-10 10:08:18 -0500

    Version Inicial

---
+++ src/main/java/com/project/claveadinamica/ClaveadinamicaApplication.java
@@ -0,0 +1,13 @@
+package com.project.claveadinamica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaveadinamicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaveadinamicaApplication.class, args);
+	}
+
+}
